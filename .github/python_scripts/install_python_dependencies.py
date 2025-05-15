5e8e785075724c2b0e154592bb340c72f5ae5668
Sébastien Goll <sebastien.goll@kitware.com>
2025-05-15 17:25:44 +0200

    CI: Update script to add current dir to pythonpath

--- .github/python_scripts/install_python_dependencies.py
+++ .github/python_scripts/install_python_dependencies.py
@@ -1,7 +1,12 @@
+from pathlib import Path
+
 import slicer.util
-from MultiverSeg.SegmentEditorMultiverSegLib import InstallLogic, DependenciesLogic
+import sys
 
 if __name__ == '__main__':
+    sys.path.append(Path(__file__).parent.joinpath("../..").resolve().as_posix())
+
+    from MultiverSeg.SegmentEditorMultiverSegLib import InstallLogic, DependenciesLogic
     DependenciesLogic.INTERACTIVE_MODE = False
     InstallLogic.INTERACTIVE_MODE = False
 
--- .github/python_scripts/install_pytorch_module.py
+++ .github/python_scripts/install_pytorch_module.py
@@ -1,8 +1,14 @@
+import sys
+from pathlib import Path
+
 import slicer.util
 
-from MultiverSeg.SegmentEditorMultiverSegLib import DependenciesLogic
+
 
 if __name__ == '__main__':
+    sys.path.append(Path(__file__).parent.joinpath("../..").resolve().as_posix())
+
+    from MultiverSeg.SegmentEditorMultiverSegLib import DependenciesLogic
     DependenciesLogic.INTERACTIVE_MODE = False
     DependenciesLogic.installPyTorchExtensionIfNeeded()
     slicer.util.quit()
